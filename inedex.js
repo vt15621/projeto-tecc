16325f5e7a442b9ca9e5b5ee4d9e8c9286b0c1c3
vitor <caldasperry2@gmail.com>
2023-11-22 21:03:43 -0300

    Versao Inicial

---
+++ inedex.js
@@ -0,0 +1,18 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+function RolarParaDireita() {
+    leonardo.style = "display:none"
+    bruna.style  = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex;margin-top:50px"
+}
+
+function RolarParaEsquerda() {
+    leonardo.style = "display: flex"
+    bruna.style = "display: none"
+    setaDireita.style = "display:flex;margin-top:50px"
+    setaEsquerda.style = "display-none"
+}
